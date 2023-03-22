b8c0a660dbf379f510534c8f989b04578e7e9d79
SorerBOT <alonfiller@gmail.com>
2023-03-23 00:48:13 +0200

    (Feat) initialised download function

---
+++ Api/Discord/downloadFile.js
@@ -0,0 +1,5 @@
+function downloadFile(fileName) {
+
+}
+
+export default downloadFile;
--- Utils/manageCommandLineArguments.js
+++ Utils/manageCommandLineArguments.js
@@ -1,4 +1,5 @@
 import uploadFile from "../Api/Discord/uploadFile.js";
+import downloadFile from "../Api/Discord/downloadFile.js";
 
 function manageCommandLineArguments(argv) {
     const uploadIndex = argv.findIndex((arg) => {
@@ -8,7 +9,21 @@ function manageCommandLineArguments(argv) {
     if (uploadIndex > -1) {
         const filePath = argv[uploadIndex + 1];
         uploadFile(filePath);
+
+        return;
     }
+
+    const downloadIndex = argv.findIndex((arg) => {
+        return (arg === "-d" || arg === "--download") ? true : false;
+    });
+
+    if (downloadIndex > -1) {
+        const filePath = argv[downloadIndex + 1];
+        downloadFile(filePath);
+
+        return;
+    }
+
 }
 
 export default manageCommandLineArguments;
